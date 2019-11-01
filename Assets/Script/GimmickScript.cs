350001b9f919fa40fd8657420cd622e397951f3e
takara0524 <shonanrgg@gmail.com>
2019-11-01 12:45:47 +0900

    UI作成・ゴール作成・ギミック作成

---
+++ Assets/Script/GimmickScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_textUI;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            m_textUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ Assets/Script/UIDestroyScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDestroyScript : MonoBehaviour {
+
+    public float timer = 3.0f;
+    void Start()
+    {
+        Destroy(gameObject, timer);
+    }
+}
