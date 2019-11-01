3ff0301f64f175a3eee75b0a55b60f46d986224e
takara0524 <shonanrgg@gmail.com>
2019-11-01 11:38:33 +0900

    距離減衰・バウンドの追加

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    private Rigidbody2D m_RB2D;
+	// Use this for initialization
+	void Start () {
+        m_RB2D = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Mathf.Abs(m_RB2D.velocity.x) < 0.5f)
+        {
+            m_RB2D.velocity *= new Vector3(0.0f, 1.0f, 1.0f) ;
+        }
+        m_RB2D.velocity *= new Vector3(0.99f, 1.0f, 1.0f);
+    }
+}
